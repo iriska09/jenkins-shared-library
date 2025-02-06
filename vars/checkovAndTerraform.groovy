d79e3eb8d6df93c24eb605610f0b4fe696406fac
iriska09 <kudaibergenovayryska09@gmail.com>
2025-02-06 12:44:25 -0600

    changed the custom policy

--- vars/checkovAndTerraform.groovy
+++ vars/checkovAndTerraform.groovy
@@ -2,22 +2,29 @@ def runCheckovAndTerraform() {
     def checkovPassed = false
     try {
         sh '''
+        echo "Starting Checkov and Terraform steps"
+        
         # Install Checkov if not already installed
         if ! checkov --version; then
+            echo "Installing Checkov"
             pip install checkov
         fi
         
         # Initialize Terraform
+        echo "Initializing Terraform"
         terraform init
         
         # Plan Terraform deployment
+        echo "Creating Terraform plan"
         terraform plan -out=plan.out
         
         # Convert the plan output to JSON
+        echo "Converting plan to JSON"
         terraform show -json plan.out > plan.json
         
         # Run Checkov (custom policies not required for now)
-        checkov -f plan.json
+        echo "Running Checkov"
+        checkov -f plan.json || (echo "Checkov failed" && exit 1)
         '''
         checkovPassed = true
     } catch (Exception e) {
@@ -25,3 +32,4 @@ def runCheckovAndTerraform() {
     }
     return checkovPassed
 }
+
