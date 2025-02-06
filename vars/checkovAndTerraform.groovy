113112620313c288b3ce32b45c341d63037b7ed5
iriska09 <kudaibergenovayryska09@gmail.com>
2025-02-06 13:24:11 -0600

    changed the config

--- vars/checkovAndTerraform.groovy
+++ vars/checkovAndTerraform.groovy
@@ -3,30 +3,44 @@ def runCheckovAndTerraform() {
     try {
         sh '''
         echo "Starting Checkov and Terraform steps"
-
-        # Check if Checkov is installed
-        if checkov --version; then
+        
+        # Create a virtual environment
+        python3 -m venv venv
+        
+        # Activate the virtual environment
+        . venv/bin/activate
+        
+        # Install Checkov
+        echo "Installing Checkov"
+        venv/bin/pip install checkov
+        
+        # Verify Checkov installation
+        if venv/bin/checkov --version; then
             echo "Checkov is installed"
         else
             echo "Checkov is not installed. Exiting."
             exit 1
         fi
-
+        
         # Initialize Terraform
         echo "Initializing Terraform"
         terraform init
-
+        
         # Plan Terraform deployment
         echo "Creating Terraform plan"
         terraform plan -out=plan.out
-
+        
         # Convert the plan output to JSON
         echo "Converting plan to JSON"
         terraform show -json plan.out > plan.json
-
-        # Run Checkov (custom policies not required for now)
+        
+        # Run Checkov
         echo "Running Checkov"
-        checkov -f plan.json || (echo "Checkov failed" && exit 1)
+        venv/bin/checkov -f plan.json || (echo "Checkov failed" && exit 1)
+        
+        # Deactivate and remove the virtual environment
+        deactivate
+        rm -rf venv
         '''
         checkovPassed = true
     } catch (Exception e) {
@@ -34,4 +48,3 @@ def runCheckovAndTerraform() {
     }
     return checkovPassed
 }
-
