d8b65f0f3330fbb2b72d141d1cf1cc0589cb422e
iriska09 <kudaibergenovayryska09@gmail.com>
2025-02-06 17:25:26 -0600

    changed checkov and terraform groovey

--- vars/checkovAndTerraform.groovy
+++ vars/checkovAndTerraform.groovy
@@ -107,7 +107,6 @@ def runCheckovAndTerraformPlan() {
 
     # Change to the directory containing Terraform configuration files
     cd ${WORKSPACE}
-    pwd
     
     # Initialize Terraform
     echo "Initializing Terraform"
