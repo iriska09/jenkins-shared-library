9f3c8f0c14b9489f2ff4a061e64458896a4cc426
iriska09 <kudaibergenovayryska09@gmail.com>
2025-02-08 15:38:08 -0600

    changed checkov and terraform groovey

--- vars/checkovAndTerraform.groovy
+++ vars/checkovAndTerraform.groovy
@@ -101,7 +101,7 @@ def runCheckovAndTerraformPlan() {
     echo "Running Checkov with Custom Policies"
     
     # **Corrected Checkov Execution with the Right Path**
-   checkov -d /var/jenkins_home/workspace/test-shared-libraries/plan2.json --external-checks-dir=${CUSTOM_POLICIES_DIR} --debug || { echo "Checkov failed! Exiting."; exit 1; }
+    checkov -d /var/jenkins_home/workspace/test-shared-libraries/ -f plan2.json --external-checks-dir=${CUSTOM_POLICIES_DIR} --debug || { echo "Checkov failed! Exiting."; exit 1; }
 
 
     '''
